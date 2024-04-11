7a97b9fa46fb55977ee9b905f4d3429eb7adc29f
dhardi <denishardi10@outlook.com>
2024-04-11 16:17:01 +0000

    add viwes

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     update_on = models.DateTimeField(auto_now=True)
+   
 
     class Meta:
         ordering = ["-created_on"]
@@ -30,5 +31,11 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    challenge = models.FloatField(default=3.0)
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
 
 
